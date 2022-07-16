df4e12ad4c89b31c5d2d421f9f3d62dbefa66934
Gento <a551.return@gmail.com>
2022-07-16 15:00:23 +0900

    react switchを削除

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Typical from "react-typical";
-import Switch from "react-switch";
 
 class Header extends Component {
   titles = [];
@@ -8,13 +7,7 @@ class Header extends Component {
   constructor() {
     super();
     this.state = { checked: false };
-    // this.onThemeSwitchChange = this.onThemeSwitchChange.bind(this);
   }
-
-  // onThemeSwitchChange(checked) {
-  //   this.setState({ checked });
-  //   this.setTheme();
-  // }
 
   setTheme() {
     var dataThemeAttribute = "data-theme";
@@ -47,46 +40,6 @@ class Header extends Component {
               <div className="title-container">
                 <HeaderTitleTypeAnimation />
               </div>
-              {/* <Switch
-                checked={this.state.checked}
-                onChange={this.onThemeSwitchChange}
-                offColor="#baaa80"
-                onColor="#353535"
-                className="react-switch mx-auto"
-                width={90}
-                height={40}
-                uncheckedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="twemoji:owl"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "20px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                checkedIcon={
-                  <span
-                    className="iconify"
-                    data-icon="noto-v1:sun-with-face"
-                    data-inline="false"
-                    style={{
-                      display: "block",
-                      height: "100%",
-                      fontSize: 25,
-                      textAlign: "end",
-                      marginLeft: "10px",
-                      color: "#353239",
-                    }}
-                  ></span>
-                }
-                id="icon-switch"
-              /> */}
             </div>
           </div>
         </div>
